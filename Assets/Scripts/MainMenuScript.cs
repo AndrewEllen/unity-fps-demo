2aaa94f09fe97a51aca0dd71b7ab8d28d2380905
Andrew Ellen <18004410@uhi.ac.uk>
2023-03-03 18:17:18 +0000

    Added script to switch menus easily Added buttons on settings and server

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -7,20 +7,8 @@ public class MainMenuScript : MonoBehaviour
     [Header("Menu's")]
     public GameObject[] menuList;
 
-    public void DisplayMainMenu() {
-        menuList[0].gameObject.SetActive(true);
-        menuList[1].gameObject.SetActive(false);
-        menuList[2].gameObject.SetActive(false);
-    }
-
-    public void DisplayServersMenu() {
-        menuList[0].gameObject.SetActive(false);
-        menuList[1].gameObject.SetActive(true);
-    }
-
-    public void DisplaySettingsMenu() {
-        menuList[0].gameObject.SetActive(false);
-        menuList[2].gameObject.SetActive(true);   
+    public void DisplayMenu(GameObject menusToSwitch) {
+        menusToSwitch.gameObject.SetActive(!menusToSwitch.gameObject.activeSelf);
     }
 
     public void ExitGame() {
