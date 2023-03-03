a97d608366a42b275962fcc32580f0e8058ff0e9
Andrew Ellen <18004410@uhi.ac.uk>
2023-03-03 17:55:58 +0000

    Added a script to change the open menu

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,9 +1,30 @@
 using UnityEngine;
 
+
 public class MainMenuScript : MonoBehaviour
 {
+
+    [Header("Menu's")]
+    public GameObject[] menuList;
+
+    public void DisplayMainMenu() {
+        menuList[0].gameObject.SetActive(true);
+        menuList[1].gameObject.SetActive(false);
+        menuList[2].gameObject.SetActive(false);
+    }
+
+    public void DisplayServersMenu() {
+        menuList[0].gameObject.SetActive(false);
+        menuList[1].gameObject.SetActive(true);
+    }
+
+    public void DisplaySettingsMenu() {
+        menuList[0].gameObject.SetActive(false);
+        menuList[2].gameObject.SetActive(true);   
+    }
+
     public void ExitGame() {
         Debug.Log("Hello");
-        Application.Quit(0);
+        Application.Quit();
     }
 }
