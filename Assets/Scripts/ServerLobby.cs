6ca715756c254bfeafc75b76625d7769f864b311
Andrew Ellen <18004410@uhi.ac.uk>
2023-03-04 21:15:57 +0000

    Added a lobby menu and code allowing joining of server

--- Assets/Scripts/ServerLobby.cs
+++ Assets/Scripts/ServerLobby.cs
@@ -13,28 +13,39 @@ public class ServerLobby : MonoBehaviour
 
     [Header("Server Name Object")]
     [SerializeField] private TextMeshProUGUI serverNameInput;
+
     [Header("Server Players Object")]
     [SerializeField] private Slider serverPlayersInput;
 
-    private Lobby currentlyHostedLobby;
+    [Header("Server Players Display Object")]
+    [SerializeField] private TextMeshProUGUI sliderValueDisplay;
+
+    [Header("Server Public Lobby Object")]
+    [SerializeField] private Toggle publicServerInput;
+
+    [Header("Server Name Object")]
+    [SerializeField] private TextMeshProUGUI serverCodeInput;
+
+    private Lobby currentlyConnectedLobby;
+    private string playerID;
 
 
     private async void Start() {
         await UnityServices.InitializeAsync();
 
         AuthenticationService.Instance.SignedIn += () => {
+            playerID = AuthenticationService.Instance.PlayerId;
             Debug.Log("logged in as user: " + AuthenticationService.Instance.PlayerId);
         };
 
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
 
-        InvokeRepeating("KeepLobbyAliveWhileHostJoined", 0f, 15f);
     }
 
-    async void asyncKeepLobbyAliveWhileHostJoined() {
-        if (currentlyHostedLobby is not null) {
+    async void KeepLobbyAliveWhileHostJoined() {
+        if (currentlyConnectedLobby is not null) {
             Debug.Log("HeartBeat sent");
-            await LobbyService.Instance.SendHeartbeatPingAsync(currentlyHostedLobby.Id);
+            await LobbyService.Instance.SendHeartbeatPingAsync(currentlyConnectedLobby.Id);
         }
     }
 
@@ -44,24 +55,71 @@ public class ServerLobby : MonoBehaviour
             string nameOfTheLobby = serverNameInput.text;
             int maxPlayersAllowedInLobby = Mathf.RoundToInt(serverPlayersInput.value);
 
-            Lobby newLobby = await LobbyService.Instance.CreateLobbyAsync(nameOfTheLobby, maxPlayersAllowedInLobby);
+            CreateLobbyOptions newLobbyOptions = new CreateLobbyOptions();
 
-            currentlyHostedLobby = newLobby;
+            newLobbyOptions.IsPrivate = !publicServerInput;
+
+            Lobby newLobby = await LobbyService.Instance.CreateLobbyAsync(nameOfTheLobby, maxPlayersAllowedInLobby, newLobbyOptions);
+
+            currentlyConnectedLobby = newLobby;
+
+            InvokeRepeating("KeepLobbyAliveWhileHostJoined", 0f, 15f);
 
             Debug.Log("Lobby Name: "+ newLobby.Name + " Lobby Max Player Count: " + newLobby.MaxPlayers);
 
         } catch (LobbyServiceException error) {
+
             Debug.Log(error);
+
         }
 
     }
 
-    public async void DisplayLobbyList() {
-        QueryResponse response = await Lobbies.Instance.QueryLobbiesAsync();
+    public void ChangeMaxPlayerHandleValue() {
+        sliderValueDisplay.text = Mathf.RoundToInt(serverPlayersInput.value).ToString();
+    }
 
+    public async void JoinLobby() {
+        if (serverCodeInput.text is not null) {
 
-        foreach (Lobby lobby in response.Results) {
-            Debug.Log("Found Lobby: " + lobby.Name + " with max player count of: " + lobby.MaxPlayers + " and ID of " + lobby.Id);
+            try {
+
+                currentlyConnectedLobby = await LobbyService.Instance.JoinLobbyByCodeAsync(serverCodeInput.text);
+
+            } catch (LobbyServiceException error) {
+
+                Debug.Log(error);
+
+            }
+        }
+        else {
+
+            try {
+
+                currentlyConnectedLobby = await LobbyService.Instance.QuickJoinLobbyAsync();
+
+            } catch (LobbyServiceException error) {
+
+                Debug.Log(error);
+
+            }
+        }
+    }
+
+    public async void LeaveLobby() {
+        try {
+
+            if (playerID == currentlyConnectedLobby.HostId) {
+                await LobbyService.Instance.DeleteLobbyAsync(currentlyConnectedLobby.Id);
+            }
+            else {
+                await LobbyService.Instance.RemovePlayerAsync(currentlyConnectedLobby.Id, playerID);
+            }
+
+        } catch (LobbyServiceException error) {
+
+            Debug.Log(error);
+
         }
     }
 
