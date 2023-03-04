db27abddee4ec6d4f2055d120b442c5247d7e6a2
Andrew Ellen <18004410@uhi.ac.uk>
2023-03-04 17:52:19 +0000

    Added text input field and slider for lobby name and max player input

--- Assets/Scripts/ServerLobby.cs
+++ Assets/Scripts/ServerLobby.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ServerLobby : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    [Header("Server Name Object")]
+    [SerializeField] private TextMeshProUGUI serverNameInput;
+    [Header("Server Players Object")]
+    [SerializeField] private Slider serverPlayersInput;
+
+    private async void Start() {
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () => {
+            Debug.Log("logged in as user: " + AuthenticationService.Instance.PlayerId);
+        };
+
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private async void CreateLobby() {
+        try {
+
+            string nameOfTheLobby = serverNameInput.text;
+            int maxPlayersAllowedInLobby = Mathf.RoundToInt(serverPlayersInput.value);
+
+            Lobby newLobby = await LobbyService.Instance.CreateLobbyAsync(nameOfTheLobby, maxPlayersAllowedInLobby);
+
+            Debug.Log("Lobby Name: "+ newLobby.Name + " Lobby Max Player Count: " + newLobby.MaxPlayers);
+
+        } catch (LobbyServiceException error) {
+            Debug.Log(error);
+        }
+
     }
+
 }
